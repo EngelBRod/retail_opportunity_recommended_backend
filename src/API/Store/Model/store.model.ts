8c2ba9cc4fb7b7d5faa9f3f5959fa3d8bfd55ab8
EngelBRod <14808288+EngelBRod@users.noreply.github.com>
2025-07-21 02:58:03 -0400

    updating store endpoint

--- src/API/Store/Model/store.model.ts
+++ src/API/Store/Model/store.model.ts
@@ -1,24 +1,54 @@
 import Store from '../../../DB/Store';
 import REGION from '../../../DB/Region';
+import { queryGetProductsByStore } from '../../Recommendations/Model/recommendations.model';
 
 export const queryGetAllStores = (place: string) => {
-
-    const selected_region = REGION.filter((value) => value.city.toLowerCase() === place.toLowerCase() || value.state.toLowerCase() === place.toLowerCase() || value.zipcode === place);
+    const selected_region = REGION.filter(
+        (value) =>
+            value.city.toLowerCase() === place.toLowerCase() ||
+            value.state.toLowerCase() === place.toLowerCase() ||
+            value.zipcode === place
+    );
     const selected_region_ids = selected_region.map((value) => value.id);
+    //const products = queryGetAllStores();
     let stores_list = [];
     if (selected_region.length) {
-        stores_list = Store.filter(
-            (value) => selected_region_ids.includes(value.region_id)
+        stores_list = Store.filter((value) =>
+            selected_region_ids.includes(value.region_id)
         );
+
+        const stores_list_os = stores_list.map((value) => {
+            const products = queryGetProductsByStore(value.id);
+            const product_sum = products.data.reduce(
+                (accumulator, currValue) =>
+                    accumulator + currValue.recommended_score,
+                0
+            );
+            const region = getRegionInfo(value.region_id);
+            const opportunity_score = product_sum / products.data.length;
+            return {
+                id: value.id,
+                region_id: value.region_id,
+                region_city: region.city,
+                region_state: region.state,
+                region_zipcode: region.zipcode,
+                name: value.name,
+                opportunity_score,
+            };
+        });
         return {
             message: 'success',
-            data: stores_list
+            data: stores_list_os,
         };
     } else {
         return {
             message: 'no data',
-            data: stores_list
+            data: stores_list,
         };
-    }    
+    }
+};
 
+const getRegionInfo = (region_id) => {
+    const region_info = REGION.filter((value) => value.id === region_id);
+    return region_info[0];
 };
